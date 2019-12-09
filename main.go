fa4b5e5205d6341f3336dd6a8bb0391bb34710b8
gw <gu123owei>
2019-12-09 12:10:56 +0800

    接口初步完成

--- main.go
+++ main.go
@@ -3,17 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/Cactush/go_test/pkg/setting"
-	"github.com/gin-gonic/gin"
+	"github.com/Cactush/go_test/routers"
 	"net/http"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "fuck boby",
-		})
-	})
+	router := routers.InitRouter()
 
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
@@ -22,6 +17,5 @@ func main() {
 		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-
 	s.ListenAndServe()
 }
